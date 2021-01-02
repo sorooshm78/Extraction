70c9ba114e1d49231d2d744ff7ef2982c73e3ffd
sorooshm78@gmail.com <sorooshm78@gmail.com>
2021-01-02 22:09:57 +0330

    add local html

--- web.py
+++ web.py
@@ -1,11 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
-import json
 
+
+# local table
+url ="<table><tr><th>First</th><th>Last</th><th>Age</th></tr><tr><td>Jill</td><td>Smith</td><td>50</td></tr><tr><td>Eve</td><td>Jackson</td><td>94</td></tr></table>"
+soup = BeautifulSoup(url, "html.parser")
+content = soup.find("table")
+
+
+# table from web
+"""
 url = "https://www.ffiec.gov/census/report.aspx?year=2011&state=01&report=demographic&msa=11500"
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
 content = soup.find("table", {"class":"main-body"})
+"""
 
 file = open("date.txt", "a")
 
@@ -13,19 +22,15 @@ file = open("date.txt", "a")
 for head in content.find_all("th"):
 	if head.string is not None:
 		file.write(head.string)
-		file.write("	")
+		file.write("\t")
 
 file.write("\n")
-
 
 # record table
 for record in content.find_all("tr"):
 	for el in record.find_all("td"):
-		if el.string is None:
-			file.write("	")
-		else:
-			file.write(el.string)
-			file.write("	 ")
+		file.write(el.string)
+		file.write("\t")
 
 	file.write("\n")
 
