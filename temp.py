87523d117c4e7606255708fdabe5fe0965e793a3
sorooshm78@gmail.com <sorooshm78@gmail.com>
2021-01-06 23:41:27 +0330

    .

--- temp.py
+++ temp.py
@@ -2,12 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import mysql.connector
 
-
 # local table
 url ="<table><tr><th>First</th><th>Last</th><th>Age</th></tr><tr><td>Jill</td><td>Smith</td><td>50</td></tr><tr><td>Eve</td><td>Jackson</td><td>94</td></tr></table>"
 soup = BeautifulSoup(url, "html.parser")
 content = soup.find("table")
-
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -17,13 +15,14 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+
 mycursor.execute("CREATE TABLE sch(id int)")
 
 # header table
 for head in content.find_all("th"):
-    if head.string is not None:
+	if head.string is not None:
 		print(head.string)
-		sql = "ALTER TABLE sch ADD (%s) char(255)"
+		sql = "ALTER TABLE sch ADD %s char(255)"
 		var = head.string
 		mycursor.execute(sql, var)
        
