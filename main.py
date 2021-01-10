4d1970983549a31ac6bf2e5260f741947660aa52
sorooshm78@gmail.com <sorooshm78@gmail.com>
2021-01-10 20:12:46 +0330

    ready for test

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def exist(sql):
 		return False
 	
 def find_file(word):
-	f = open("pass.txt", "r")
+	f = open("info.txt", "r")
 	txt = f.read()
 	s = re.findall(word + ".*=.*", txt)
 	return s[0].replace(" ","").replace(word + "=","")
@@ -67,7 +67,7 @@ if exist(sql_command):
 	print("database exist")
 else:
 	print("database not exist")
-	sql_command = "CREATE DATABASE %s" %(database_name)
+	sql_command = "CREATE DATABASE `%s`" %(database_name)
 	print(sql_command)
 	mycursor.execute(sql_command)
 
@@ -83,24 +83,24 @@ if exist(sql_command):
 else:
 	print("table not exist")
 	id = header[0].string
-	sql_command = "CREATE TABLE %s(%s VARCHAR(255))" %(table_name, id)
+	sql_command = "CREATE TABLE `%s` (`%s` VARCHAR(255))" %(table_name, id)
 	print(sql_command)
 	mycursor.execute(sql_command)
 
 print("*************************** check name exist ***************************")
 
-for x in range(1, len(record) - 1):
+for x in range(1, len(record)):
 	el = record[x].find_all("td")
 	val_id = el[0].string
 	id = header[0].string
 
-	sql = "SELECT %s FROM %s" %(id, table_name)
+	sql = "SELECT `%s` FROM `%s`" %(id, table_name)
 
 	if check_exist(sql, val_id) == True:
 		print(val_id + " is exist")
 	else:
 		print(val_id + " is not exist")	
-		sql_command = "INSERT INTO %s (%s) VALUES (\"%s\")"% (table_name, id, val_id)
+		sql_command = "INSERT INTO `%s` (`%s`) VALUES (\"%s\")"% (table_name, id, val_id)
 		print(sql_command)
 		mycursor.execute(sql_command)
 		mydb.commit()
@@ -108,29 +108,30 @@ for x in range(1, len(record) - 1):
 
 print("*************************** insert header table ***************************")
 
-for x in range(1, len(header) - 1):
+print("len head" ,len(header))
+
+for x in range(1, len(header)):
 		column = header[x].string
-		sql = "SHOW COLUMNS FROM %s LIKE \"%s\"" %(table_name, column)
+		sql = "SHOW COLUMNS FROM `%s` LIKE \"%s\"" %(table_name, column)
 
 		if exist(sql) == True:
 			print(column + " column is exist")
 		else:
 			print(column + " column is not exist")
-			sql_command = "ALTER TABLE %s ADD COLUMN `%s` VARCHAR(255)" % (table_name, column)
+			sql_command = "ALTER TABLE `%s` ADD COLUMN `%s` VARCHAR(255)" % (table_name, column)
 			print(sql_command)
 			mycursor.execute(sql_command)
 			mydb.commit()
 
 print("*************************** insert record table ***************************")
-for x in range(1, len(record) - 1):
+for x in range(1, len(record)):
 	el = record[x].find_all("td")
 	val_id = el[0].string
 
 	for y in range(1, len(el)):	
 		column = header[y].string
 		val_column = el[y].string
-#		sql_command = "UPDATE %s SET %s = \"%s\" WHERE %s = \"%s\"" % (table_name, column, val_column, id, val_id)
-		sql_command = "UPDATE %s SET `%s` = \"%s\" WHERE %s = \"%s\"" % (table_name, column, val_column, id, val_id)
+		sql_command = "UPDATE `%s` SET `%s` = \"%s\" WHERE `%s` = \"%s\"" % (table_name, column, val_column, id, val_id)
 
 		print(sql_command)
 		mycursor.execute(sql_command)
